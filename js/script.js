bd854cc7d6485af5a7cda7a2e0625ab81d94eaa4
1AinCraD1 <118106610+1AinCraD1@users.noreply.github.com>
2023-12-09 18:24:55 +0200

    Create script.js

---
+++ js/script.js
@@ -0,0 +1,19 @@
+function redirectToPage(page) {
+    window.open(page, '_self');
+}
+
+$('#buttonLR7').on('click', function() {
+    redirectToPage('ЛР 7.html');
+});
+
+$('#buttonLR8').on('click', function() {
+    redirectToPage('ЛР 8.html');
+});
+
+$('#buttonLR9').on('click', function() {
+    redirectToPage('ЛР 9.html');
+});
+
+$('#buttonLR10').on('click', function() {
+    redirectToPage('ЛР 10.html');
+});
